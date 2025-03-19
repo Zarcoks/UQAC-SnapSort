0c40a6af571371013416a6413aa53a3edaa2d92b
victrolles <95492416+victrolles@users.noreply.github.com>
2025-03-19 18:39:59 -0400

    DX improvement

--- snapsort/src/electron/main.ts
+++ snapsort/src/electron/main.ts
@@ -1,17 +1,24 @@
 import {app, BrowserWindow} from 'electron';
 import path from 'path';
+import { isDev } from './util.js';
 
 type test = string;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+  // const mainWindow = new BrowserWindow({
+  //   width: 800,
+  //   height: 600,
+  //   webPreferences: {
+  //     nodeIntegration: true
+  //   }
+  // });
+  const mainWindow = new BrowserWindow();
 
-  mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+  if (isDev()) {
+    mainWindow.loadURL('http://localhost:5173');
+  }
+  else {
+    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+  }
 });
 
